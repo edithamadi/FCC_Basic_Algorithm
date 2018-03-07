a63740deec3472d7ef82239bb082024772d42d54
andyweru <andyweru@gmail.com>
2018-03-07 19:44:23 +0300

    repeat a given string n number of times

---
+++ repeatString.js
@@ -0,0 +1,14 @@
+// Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
+
+
+function repeatStringNumTimes(str, num) {
+  if(num < 0){
+    return "";
+  }
+
+  var newString = str.repeat(num);
+
+  return newString;
+}
+
+repeatStringNumTimes("abc", 3);
