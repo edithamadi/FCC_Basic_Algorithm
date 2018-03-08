e1e551d9abcf7c1af6c378b1f7dc9bd1b8b8dc9b
andyweru <andyweru@gmail.com>
2018-03-08 12:29:00 +0300

    return the lowest index at whick a value should be inserted

---
+++ whereDoIBelong.js
@@ -0,0 +1,14 @@
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+function getIndexToIns(arr, num) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+
+  for (var a = 0; a < arr.length; a++) {
+    if (arr[a] >= num)
+      return a;
+  }
+
+  return arr.length;
+}
